fb5b8f2ef89954d0a3bc03a99f2a3dc4373aea66
Philippe Bruhat (BooK) <book@cpan.org>
2021-09-10 10:41:28 +0200

    Parse tokens into expressions

--- lib/Perlisp.pm
+++ lib/Perlisp.pm
@@ -13,4 +13,26 @@ sub tokenize( $self, $string ) {
     return grep $_, split / +/, $string =~ s{([()])}{ $1 }gr;
 }
 
+sub parse_expression ( $self, @list ) {
+    return if !@list;    # done
+
+    my $token = shift @list;
+    if ( $token eq ')' ) {
+        die "closing paren found\n";
+    }
+
+    my @expr;
+    my $type;
+    if ( $token =~ /\A-?[0-9]/ ) {    # starts with a number, surely it's one
+        $token += 0;                  # well, force perl to make it a number
+        $type = 'number';
+    }
+    else {                            # everything else is a symbol
+        $type = 'symbol';
+    }
+    push @expr, [ $type => $token ];
+
+    return @expr;
+}
+
 1;
