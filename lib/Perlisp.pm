deb1780a0f7cf946c427a67ff42e10669a653540
Philippe Bruhat (BooK) <philippe.bruhat@booking.com>
2022-01-21 10:13:36 +0100

    Create a basic but working `evaluate`

---
+++ lib/Perlisp.pm
@@ -0,0 +1,62 @@
+package Perlisp;
+
+use 5.024;
+use warnings;
+no warnings 'experimental::signatures';
+use feature 'signatures';
+
+package Perlisp::Expr;
+
+sub new {
+    my ($class, $type, $value ) = @_;
+    return bless [ $type => $value ], $class;
+}
+
+sub type ( $expr ) { return $expr->[0] };
+sub value ( $expr )  { return $expr->[1] };
+
+package Perlisp;
+
+sub is_atom ($expr) {
+    my $type = $expr->type;
+    return
+         $type eq 'Symbol'
+      || $type eq 'Number'
+      || $type eq 'String'
+      || $type eq 'Boolean';
+}
+
+sub is_symbol ($expr) { return $expr->type eq 'Symbol'; }
+
+sub lookup ($expr, $env) {
+    if ( exists $env->{$expr->value} ) {
+        return $env->{$expr->value};
+    }
+    else {
+        ...;
+    }
+}
+
+sub evaluate ( $expr, $env ) {
+    if ( is_atom($expr) ) {
+        if ( is_symbol($expr) ) {
+            return lookup( $expr, $env );
+        }
+        else {
+            return $expr;
+        }
+    }
+    else {
+        ...;
+    }
+}
+
+
+say evaluate( Perlisp::Expr->new( Symbol => 'foo' ), { foo => 1 } );
+say evaluate( Perlisp::Expr->new( Symbol => 'foo' ), {} );
+
+1;
+
+__END__
+
+"foo" -not this-> Atom("foo") -but this-> is_atom(Atom("foo"))
